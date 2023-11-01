94a193a20370a987fa7c261709538144d485f05a
Elenita.dwg <89352049+ElenaEF00@users.noreply.github.com>
2023-11-01 11:10:27 +0100

    Add files via upload

---
+++ 31-10-23/script.js
@@ -0,0 +1,14 @@
+const animals = [
+  "cat",
+  "dog",
+  "giraffe",
+  "parrot",
+  "elephant",
+  "bird",
+  "snake",
+  "fish",
+];
+
+for (let i = 0; i < animals.length; i++) {
+  console.log(i + " - " + animals[i]);
+}

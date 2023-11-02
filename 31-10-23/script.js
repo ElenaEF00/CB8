642afc79bf91778cc7ddb831bba3b24fbb39f247
Elenita.dwg <89352049+ElenaEF00@users.noreply.github.com>
2023-11-02 18:53:31 +0100

    Update script.js

--- 31-10-23/script.js
+++ 31-10-23/script.js
@@ -12,3 +12,9 @@ const animals = [
 for (let i = 0; i < animals.length; i++) {
   console.log(i + " - " + animals[i]);
 }
+
+// esercizio corretto per far partire l'elenco da 0 e non da 1
+
+for (let i = 0; i < animals.lenght, i++) {
+  console.log (i+1 + " - " animals[i]
+}

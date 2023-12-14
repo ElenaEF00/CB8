d6ff62bcb6712e61060511e6058390a83df06f5d
Elenita.dwg <89352049+ElenaEF00@users.noreply.github.com>
2023-12-14 17:08:09 +0100

    Add files via upload

---
+++ 13-12-23/src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <img src="https://img.logoipsum.com/331.svg" alt="logo" />
+      <ul className="footer__list">
+        <li>Termini di utilizzo</li>
+        <li>Informativa sulla privacy</li>
+        <li>Aiuto</li>
+        <li>© 1996-2023, Amazon.com, Inc. o società affiliate</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
